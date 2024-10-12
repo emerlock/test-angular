fbb70678e63efac34c273cc6d6e6b5a970f26443
emerlock <5019530+emerlock@users.noreply.github.com>
2024-10-11 21:10:56 -0400

    add input and updates for json

--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -9,24 +9,47 @@ import { Component } from '@angular/core';
 })
 export class TestPageComponent {
 
-  //json object array
+  ssn: string = "";
+
+//json object array
   jsonArray = [
     {
       "title": "hello1",
       "body": "this is a body1",
-      "number": 31
+      "number": 31,
+      "ssn": this.ssn
     },
     {
       "title": "hello2",
       "body": "this is a body2",
-      "number": 32
+      "number": 32,
+      "ssn": this.ssn
     },
     {
       "title": "hello3",
       "body": "this is a body3",
-      "number": 33
+      "number": 33,
+      "ssn": this.ssn
     },
 
   ]
+
+  // CHANGE EVENT FROM KEYUP IN TEMPLATE
+  changeEvent(event: any){
+    this.ssn = event.target.value;
+
+    //CALL CHANGEJSON TO UPDATE OBJECT
+    this.changeJSON(event.target.value);
+    console.log('changed! ', event.target.value );
+  }
+
+  //UPDATES JSON OBJECT
+  changeJSON(ssn: any){
+    this.jsonArray.forEach(item => {
+      item.ssn = ssn
+    });
+
+  }
+
 
 }
